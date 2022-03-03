47fb1f1640aadd6e36f62d938cd91bdd70f9601b
Mohab Mohamed <mohab8m@gmail.com>
2022-03-02 20:17:35 -0500

    add disjoint graph

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,9 @@
   import Icon from '$lib/atoms/icon/icon.svelte';
   import { default as cloud } from '$lib/assets/icons/cloud';
   import Button from '$lib/atoms/button/button.svelte';
+  import DisjointCloud from '$lib/organisms/disjoint-cloud/disjoint-cloud.svelte';
 </script>
 
 <Icon data={cloud} />
 <Button label={'mohab'} />
+<DisjointCloud />
