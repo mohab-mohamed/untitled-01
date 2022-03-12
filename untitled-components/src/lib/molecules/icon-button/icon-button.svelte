2fdf454a0f91f35b085d86828425336cfb16171a
Mohab Mohamed <mohab8m@gmail.com>
2022-03-11 20:43:56 -0500

    add property to icon-button for scaling

--- untitled-components/src/lib/molecules/icon-button/icon-button.svelte
+++ untitled-components/src/lib/molecules/icon-button/icon-button.svelte
@@ -2,11 +2,12 @@
     import Icon from '../../atoms/icon/icon.svelte';
 
     export let iconData;
+    export let iconScale;
 
     //optional
     export let className = undefined;
 </script>
 
 <button class={['p-5 hover:bg-gray-100 rounded-lg', className].join(' ')}>
-    <Icon data={iconData} scale={3} />
+    <Icon data={iconData} scale={iconScale} />
 </button>
