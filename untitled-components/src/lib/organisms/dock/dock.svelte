6e9fe94ef3ea639a8371cd70092b0d3c84b97542
Mohab Mohamed <mohab8m@gmail.com>
2022-03-11 20:45:08 -0500

    assign scale within dock

--- untitled-components/src/lib/organisms/dock/dock.svelte
+++ untitled-components/src/lib/organisms/dock/dock.svelte
@@ -5,6 +5,6 @@
 
 <div class="flex items-center h-18">
     {#each list as item}
-        <IconButton className="mx-3" iconData={item.iconData} />
+        <IconButton className="mx-3" iconData={item.iconData} iconScale={2} />
     {/each}
 </div>
